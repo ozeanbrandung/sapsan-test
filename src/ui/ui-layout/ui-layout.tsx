b9f47c68dc8beb20dacff047b9fca49780c19553
ozeanbrandung <natalia.v.molchanova@yandex.ru>
2025-03-09 13:40:37 +0400

    try to fix ts error by removing ref props at layout

--- src/ui/ui-layout/ui-layout.tsx
+++ src/ui/ui-layout/ui-layout.tsx
@@ -1,19 +1,19 @@
 import clsx from 'clsx';
-import { JSX, ReactNode, Ref } from 'react';
+import { JSX, ReactNode /*, Ref*/ } from 'react';
 
 interface IUILayoutProps {
   children: ReactNode;
   className?: string;
   tag?: keyof JSX.IntrinsicElements;
-  ref?: Ref<HTMLElement>;
+  //ref?: Ref<HTMLElement>;
 }
 export function UILayout(props: IUILayoutProps): ReactNode {
-  const { className, children, tag = 'div', ref, ...rest } = props;
+  const { className, children, tag = 'div', /*ref,*/ ...rest } = props;
   const Tag = tag;
 
   return (
     <Tag
-      ref={ref}
+      //ref={ref}
       className={clsx('px-[16px] lg:px-[0px] sm:max-w-[390px] lg:max-w-[1265px] m-auto', className)}
       {...rest}
     >
